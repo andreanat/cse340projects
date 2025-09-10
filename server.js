a02fe518774bd7e324619212e5b47efa978591da
andreanat <andrea.nmb13@gmail.com>
2025-09-09 21:18:25 -0600

    aa

--- server.js
+++ server.js
@@ -43,5 +43,5 @@ app.get("/", (req, res) => {
 })
 
 // --- Server ---
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`app listening on localhost:${PORT}`))
