7c8ba0d8fef702b0140f94e113444054c26a287d
andreanat <andrea.nmb13@gmail.com>
2026-03-18 10:55:07 -0600

    done

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,4 +1,5 @@
 const invModel = require("../models/inventory-model")
+const utilities = require("../utilities/")
 
 const invController = {}
 
@@ -6,9 +7,11 @@ invController.buildByClassificationId = async function (req, res) {
   const classification_id = req.params.classificationId
 
   const data = await invModel.getInventoryByClassificationId(classification_id)
+  const nav = await utilities.getNav()
 
   res.render("inventory/classification", {
     title: "Vehicles",
+    nav,
     data,
   })
 }
