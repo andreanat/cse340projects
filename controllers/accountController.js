a7d1dd47148ece3086b5059c789828c37dd9bede
andreanat <andrea.nmb13@gmail.com>
2026-04-04 15:45:41 -0600

    register

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -53,11 +53,23 @@ async function registerAccount(req, res) {
     account_password
   } = req.body
 
+  let hashedPassword
+  try {
+    hashedPassword = await bcrypt.hash(account_password, 10)
+  } catch (error) {
+    req.flash("notice", "Sorry, there was an error processing your request.")
+    return res.status(500).render("account/register", {
+      title: "Register",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -65,14 +77,14 @@ async function registerAccount(req, res) {
       "notice",
       `Congratulations, you're registered ${account_firstname}. Please log in.`
     )
-    res.status(201).render("account/login", {
+    return res.status(201).render("account/login", {
       title: "Login",
       nav,
       errors: null,
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
+    return res.status(501).render("account/register", {
       title: "Register",
       nav,
       errors: null,
